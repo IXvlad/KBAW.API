cdf9fbf98548c054943adff7042f4f5c85da586d
Vladislav Meledin <meledin.94@mail.ru>
2022-02-18 21:24:44 +0700

    Create domain models

---
+++ KBAW.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DomainModels.DomainsModel;
+
+namespace DomainModels.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<IAsyncEnumerable<T>> GetAll();
+        
+        Task<T> GetById(int id);
+        
+        Task Create(T item);
+        
+        Task Update(T item);
+        
+        Task Delete(T item);
+    }
+}
