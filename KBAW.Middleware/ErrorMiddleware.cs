5bbe5d9d69a28f00c5867199aafed308d8690aaa
Vladislav Meledin <dmd.vladislav.meledin@vwfspay.com>
2022-05-17 16:12:54 +0700

    Add global error handler

---
+++ KBAW.Middleware/ErrorMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace KBAW.ErrorHandler
+{
+    public class ErrorMiddleware
+    {
+        private readonly RequestDelegate _nextMiddleware;
+
+        public ErrorMiddleware(RequestDelegate nextMiddleware)
+        {
+            _nextMiddleware = nextMiddleware;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _nextMiddleware(context);
+            }
+            catch (Exception error)
+            {
+                HttpResponse response = context.Response;
+                response.ContentType = "application/json";
+
+                response.StatusCode = error switch
+                {
+                    CustomApplicationException => (int)HttpStatusCode.BadRequest,
+                    KeyNotFoundException => (int)HttpStatusCode.NotFound,
+                    _ => (int)HttpStatusCode.InternalServerError
+                };
+
+                string result;
+                
+                if (error is CustomApplicationException applicationException)
+                {
+                    CustomApplicationException exception = applicationException ?? throw new InvalidOperationException();
+
+                    result = JsonSerializer.Serialize(new
+                    {
+                        exception.Message,
+                        exception.InnerException,
+                        exception.Data,
+                        exception.Detail,
+                        response.StatusCode
+                    });
+                }
+                else
+                {
+                    result = JsonSerializer.Serialize(new
+                    {
+                        error.Message,
+                        error.InnerException,
+                        error.Data,
+                        response.StatusCode
+                    });
+                }
+                
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
