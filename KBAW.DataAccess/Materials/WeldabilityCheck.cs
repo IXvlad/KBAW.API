8c699ab4fff4491c419725c6c83bc07f2b1b8e30
Vladislav Meledin <dmd.vladislav.meledin@vwfspay.com>
2022-05-25 14:37:29 +0700

    add welding check

---
+++ KBAW.DataAccess/Materials/WeldabilityCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using KBAW.Utils;
+
+namespace KBAW.DataAccess.Materials
+{
+    public static class WeldabilityCheck
+    {
+        public enum WeldingResult
+        {
+            [Display(Name = "Quality connection")]
+            QualityСonnection = 1,
+            [Display(Name = "Poor quality connections")]
+            PoorQualityConnections,
+            [Display(Name = "Not data or not explored")]
+            NotData
+        }
+
+        public static WeldingResult GetWeldingResult(this int[,] table, Materials material1, Materials material2)
+        {
+            int row = table.GetRow().FindIndex((int)material1)[0];
+            int column = table.GetColumn().FindIndex((int)material2)[0];
+            
+            return (WeldingResult)table[row, column];
+        }
+    }
+}
