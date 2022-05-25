e2d1bab392c37663ff9af426ddf9f77c75955baa
Vladislav Meledin <dmd.vladislav.meledin@vwfspay.com>
2022-05-25 14:04:50 +0700

    add weldability tables

---
+++ KBAW.DataAccess/Materials/Tables/MaterialCombinationIPW.cs
@@ -0,0 +1,44 @@
+﻿namespace KBAW.DataAccess.Materials.Tables
+{
+    public class MaterialCombinationIPW
+    {
+        // 1 - Quality connection
+        // 2 - Poor quality connections
+        // 3 - Not data or not explored
+
+        private int[,] _table = new int[,]
+        {
+            {
+                -1, (int)Materials.AlAlloys, (int)Materials.CuZn, (int)Materials.Bronze, (int)Materials.CarbidesCemented, (int)Materials.CoAlloys,
+                (int)Materials.Columbium, (int)Materials.Cu, (int)Materials.CuNi, (int)Materials.Pb, (int)Materials.MgAlloys, (int)Materials.Mo,
+                (int)Materials.NiAlloys, (int)Materials.SteelAlloy, (int)Materials.SteelCarbon, (int)Materials.SteelFreeMachG, (int)Materials.SteelMaraging,
+                (int)Materials.SteelSintered, (int)Materials.SteelStainless, (int)Materials.SteelTool, (int)Materials.Ta, (int)Materials.TiAlloys,
+                (int)Materials.W, (int)Materials.ValveMaterials, (int)Materials.ZrAlloys,
+            },
+            { (int)Materials.AlAlloys,         1, 2, 2, 3, 2, 3, 2, 2, 3, 3, 3, 2, 2, 2, 3, 3, 3, 2, 3, 3, 2, 3, 3, 2 },
+            { (int)Materials.CuZn,             2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Bronze,           2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.CarbidesCemented, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3 },
+            { (int)Materials.CoAlloys,         2, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Columbium,        3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Cu,               2, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 2, 3, 3, 1, 3, 3, 1 },
+            { (int)Materials.CuNi,             2, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 1, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Pb,               3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.MgAlloys,         3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Mo,               3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3 },
+            { (int)Materials.NiAlloys,         2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 2, 1, 1, 1, 1, 3, 3, 3, 2, 3 },
+            { (int)Materials.SteelAlloy,       2, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 1, 1, 1, 2, 1, 1, 1, 1, 3, 1, 3, 1, 3 },
+            { (int)Materials.SteelCarbon,      2, 3, 1, 1, 1, 3, 1, 1, 3, 3, 3, 1, 1, 1, 2, 1, 1, 1, 1, 3, 1, 3, 1, 3 },
+            { (int)Materials.SteelFreeMachG,   3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 2, 2, 3, 3, 3, 2, 3 },
+            { (int)Materials.SteelMaraging,    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 2, 3, 1, 1, 1, 3, 3, 3, 1, 3 },
+            { (int)Materials.SteelSintered,    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 3 },
+            { (int)Materials.SteelStainless,   2, 3, 3, 3, 3, 3, 2, 1, 3, 3, 3, 1, 1, 1, 2, 1, 1, 2, 1, 3, 3, 1, 1, 3 },
+            { (int)Materials.SteelTool,        3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 2, 1, 1, 1, 2, 3, 3, 3, 1, 3 },
+            { (int)Materials.Ta,               3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3 },
+            { (int)Materials.TiAlloys,         2, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 3, 3, 1, 3, 3, 3 },
+            { (int)Materials.W,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3 },
+            { (int)Materials.ValveMaterials,   3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1, 1, 2, 1, 1, 1, 1, 3, 3, 3, 1, 3 },
+            { (int)Materials.ZrAlloys,         2, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1 },
+        };
+    }
+}
---
+++ KBAW.DataAccess/Materials/Tables/WeldabilityTableMaterials.cs
@@ -0,0 +1,58 @@
+﻿namespace KBAW.DataAccess.Materials.Tables
+{
+    public class WeldabilityTableMaterials
+    {
+        // 1 - Quality connection
+        // 2 - Poor quality connections
+        // 3 - Not data or not explored
+
+        private int[,] Table = new int[,]
+        {
+            {
+                -1, (int)Materials.Al, (int)Materials.AlAlloys, (int)Materials.CuZn, (int)Materials.Bronze, (int)Materials.CdO,
+                (int)Materials.CastIron, (int)Materials.Ceramics, (int)Materials.Co, (int)Materials.Cu, (int)Materials.CuNi,
+                (int)Materials.FeSintered, (int)Materials.FeNi, (int)Materials.Pb, (int)Materials.Mg, (int)Materials.MgAlloys,
+                (int)Materials.Mo, (int)Materials.Monel, (int)Materials.Ni, (int)Materials.NiAlloys, (int)Materials.Nimonic,
+                (int)Materials.Nb, (int)Materials.NbAlloys, (int)Materials.Ag, (int)Materials.AgAlloys, (int)Materials.SteelCarbon,
+                (int)Materials.SteelAlloy, (int)Materials.SteelMaraging, (int)Materials.SteelStainless, (int)Materials.Ta, (int)Materials.Th,
+                (int)Materials.Ti, (int)Materials.Ws, (int)Materials.SuperhardMaterial, (int)Materials.U, (int)Materials.V, (int)Materials.ZrAlloys
+            },
+            { (int)Materials.Al,                1, 1, 1, 3, 3, 3, 1, 3, 1, 3, 3, 3, 3, 3, 1, 3, 3, 1, 3, 3, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 1, 3, 1, 3, 3, 1 },
+            { (int)Materials.AlAlloys,          1, 1, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.CuZn,              1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Bronze,            3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.CdO,               3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.CastIron,          3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Ceramics,          1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Co,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Cu,                1, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 1, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.CuNi,              3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.FeSintered,        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.FeNi,              3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Pb,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Mg,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.MgAlloys,          1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Mo,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Monel,             3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Ni,                1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.NiAlloys,          3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Nimonic,           3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Nb,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.NbAlloys,          3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Ag,                3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.AgAlloys,          3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.SteelCarbon,       1, 2, 3, 3, 3, 1, 3, 3, 1, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 2, 3, 3, 3 },
+            { (int)Materials.SteelAlloy,        1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 1, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.SteelMaraging,     3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.SteelStainless,    1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 1, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 2, 3, 3, 3, 3, 2 },
+            { (int)Materials.Ta,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3 },
+            { (int)Materials.Th,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.Ti,                1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1, 3, 1, 3, 3, 3, 3, 3 },
+            { (int)Materials.Ws,                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3 },
+            { (int)Materials.SuperhardMaterial, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.U,                 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3 },
+            { (int)Materials.V,                 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { (int)Materials.ZrAlloys,          1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3 },
+        };
+    }
+}
