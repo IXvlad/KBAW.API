7d4795838a3b2d75bec61e600cdb7fa36d45264f
meled <meledin.94@mail.ru>
2022-05-28 15:41:49 +0700

    Some changes

--- KBAW.API/Controlles/RecordsController.cs
+++ KBAW.API/Controlles/RecordsController.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using AutoMapper;
-using KBAW.API.ViewModels;
 using KBAW.Query.EFServices.Interfaces;
+using KBAW.Query.EFServices.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace KBAW.API.Controlles
 {
@@ -13,42 +10,18 @@ namespace KBAW.API.Controlles
     [Route("[controller]")]
     public class RecordsController : ControllerBase
     {
-        private readonly Lazy<IArticleQueryService> _articleQueryService;
-        private readonly Lazy<IBookQueryService> _bookQueryService;
-        private readonly Lazy<IPatentQueryService> _patentQueryService;
-        private readonly Lazy<IMapper> _mapper;
+        private readonly Lazy<IGetAllRecordsCommand> _getAllRecordsCommand;
 
         public RecordsController(
-            Lazy<IArticleQueryService> articleQueryService, 
-            Lazy<IBookQueryService> bookQueryService, 
-            Lazy<IPatentQueryService> patentQueryService, 
-            Lazy<IMapper> mapper)
+            Lazy<IGetAllRecordsCommand> getAllRecordsCommand)
         {
-            _articleQueryService = articleQueryService;
-            _bookQueryService = bookQueryService;
-            _patentQueryService = patentQueryService;
-            _mapper = mapper;
+            _getAllRecordsCommand = getAllRecordsCommand;
         }
 
         [HttpGet("GetAllRecords")]
         public IEnumerable<Record> GetAllRecords()
         {
-            IQueryable<Record> articles = _articleQueryService.Value.GetAll()
-                .Include(x => x.AuthorOfArticles)
-                .ThenInclude(x => x.Author)
-                .Select(article => _mapper.Value.Map<Record>(article));
-            
-            IQueryable<Record> books = _bookQueryService.Value.GetAll()
-                .Include(x => x.AuthorOfBooks)
-                .ThenInclude(x => x.Author)
-                .Select(book => _mapper.Value.Map<Record>(book));
-            
-            IQueryable<Record> patents = _patentQueryService.Value.GetAll()
-                .Include(x => x.AuthorOfPatents)
-                .ThenInclude(x => x.Author)
-                .Select(patent => _mapper.Value.Map<Record>(patent));
-
-            return Enumerable.Concat(articles, books).Concat(patents);
+            return _getAllRecordsCommand.Value.Get();
         }
     }
 }
