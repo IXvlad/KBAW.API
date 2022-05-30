f52982d2c506645e68d99d073dd1538e914785d4
Vladislav Meledin <dmd.vladislav.meledin@vwfspay.com>
2022-05-30 12:48:41 +0700

    Add weldability controller

---
+++ KBAW.Utils/EnumHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace KBAW.Utils
+{
+    public static class EnumHelper
+    {
+        public static string DisplayName(this Enum value)
+        {
+            return value.GetType()
+                .GetMember(value.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                ?.GetName()!;
+        }
+    }
+}
