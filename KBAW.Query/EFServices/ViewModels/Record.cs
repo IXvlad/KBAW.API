df3cdd888eca3d1e0e181c87cdcf430febda98b6
meled <meledin.94@mail.ru>
2022-06-11 19:48:43 +0700

    Add GetAllRecords command

---
+++ KBAW.Query/EFServices/ViewModels/Record.cs
@@ -0,0 +1,22 @@
+﻿namespace KBAW.Query.EFServices.ViewModels;
+
+public class Record
+{
+    public string Country { get; set; }
+        
+    public string Description { get; set; }
+        
+    public string Title { get; set; }
+        
+    public string Ydk { get; set; }
+        
+    public string? Classification { get; set; }
+        
+    public string? Number { get; set; }
+        
+    public string? PlacePublication { get; set; }
+        
+    public string? СertificateAuthorship { get; set; }
+        
+    public string[] Authors { get; set; }
+}
