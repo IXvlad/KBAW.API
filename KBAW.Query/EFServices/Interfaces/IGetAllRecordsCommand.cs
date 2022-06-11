63731f0d391353e7b3cc3377b2d5c2afd18ba602
meled <meledin.94@mail.ru>
2022-06-11 19:48:43 +0700

    Add GetAllRecords command

---
+++ KBAW.Query/EFServices/Interfaces/IGetAllRecordsCommand.cs
@@ -0,0 +1,9 @@
+﻿using KBAW.Container.Dependencies;
+using KBAW.Query.EFServices.ViewModels;
+
+namespace KBAW.Query.EFServices.Interfaces;
+
+public interface IGetAllRecordsCommand : IScoped
+{
+    IEnumerable<Record> Get();
+}
